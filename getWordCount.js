847efa768483668d47ecca623552230a9377940e
GrethelRivera <grethelr91@gmail.com>
2022-06-17 23:49:49 -0400

    first commit

---
+++ getWordCount.js
@@ -0,0 +1,11 @@
+function countWords(str) {
+    let count = str.split(' ').length;
+    return count;
+  }
+  
+  console.log(countWords("Have a wonderful day"));
+
+
+
+// Create a method that takes a string and returns the word count. 
+// The string will be a sentence.
---
+++ numberHigher.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+
+
+// Write a function that returns true if there exists at least one number that is larger than or equal to n.
