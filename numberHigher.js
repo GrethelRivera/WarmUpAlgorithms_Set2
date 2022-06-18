397dcac758a6fef71c39812c747690df14216f1b
GrethelRivera <grethelr91@gmail.com>
2022-06-18 12:22:32 -0400

    first commit

--- numberHigher.js
+++ numberHigher.js
@@ -1,10 +1,15 @@
+function existsHigher(arr, n) {
+	for (i = 0; i < arr.length; i++){
+		
+	if (arr[i] >= n ) {
+		return true;
+	}
+	}
+	return false;
+}
 
 
 
 
-
-
-
-
-
-// Write a function that returns true if there exists at least one number that is larger than or equal to n.
+// Write a function that returns true if there exists at least one number 
+//that is larger than or equal to n.
