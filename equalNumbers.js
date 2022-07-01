647eca7929b83f7463791ffbbf43729f2a8ee55d
GrethelRivera <grethelr91@gmail.com>
2022-07-01 12:08:19 -0400

    first commit

---
+++ equalNumbers.js
@@ -0,0 +1,13 @@
+function isSameNum(num1, num2) {
+	if (num1 === num2){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+console.log(isSameNum(2,2))
+
+
+// Create a function that returns true when x is equal
+//  to y; otherwise return false.
