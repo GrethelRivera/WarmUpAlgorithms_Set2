49f81041bb3be64e6c38d753306912dfdfd4e476
GrethelRivera <grethelr91@gmail.com>
2022-07-03 09:40:16 -0400

    first commit

---
+++ nameGreeting.js
@@ -0,0 +1,13 @@
+function greet(name) {
+
+    return "Hello" + " " + name + "!";
+}
+
+console.log(greet("Grethel"))
+//Returns Hello Grethel!
+
+
+
+
+
+// Create a function that takes a name and returns a greeting in the form of a string.
