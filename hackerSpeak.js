fde277cea5db5fe544f83a6cc6d7fa42a24369e1
GrethelRivera <grethelr91@gmail.com>
2022-07-05 14:47:25 -0400

    first commit

---
+++ hackerSpeak.js
@@ -0,0 +1,10 @@
+function hackerSpeak(str) {
+    var chars = {'a':'4','e':'3','i':'1', 'o':'0', 's':'5'};
+    str = str.replace(/[aeios]/g, letter => chars[letter]);
+    
+    return str;
+  }
+
+  console.log(hackerSpeak("javascript is cool"))
+
+  //j4v45cr1pt 15 c00l 
